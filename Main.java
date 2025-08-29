9566c2680580ff4afb0f950a18c62b9805176961
ychasser <41200686+ychasser@users.noreply.github.com>
2025-08-29 22:56:49 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,90 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Jeu du pendu");
+    // les mots à découvrir
+    String[] tMots = { "algorithme", "attribut", "javascript", "programmation" };
+
+  }
+
+  /*
+   * fonction : motADecouvrir -> choisir au hasard un mot au hasard
+   * données : 
+   * résultats : 
+   */
+
+
+  /*
+   * fonction : crypterMot -> crypter un mot
+   * données : 
+   * résultats : 
+   */
+
+
+  /*
+   * fonction : estDans -> une lettre est dans un mot ?
+   * données : 
+   *           
+   * résultats : 
+   */
+
+
+  /*
+   * fonction : actualiserMotCrypte -> actualiser le mot crypté
+   * données : 
+   *          
+   *           
+   * résultats : 
+   *           
+   *           
+   */
+ 
+
+  /*
+   * fonction : motComplet -> mot décrypté entièrement ?
+   * données : 
+   * résultats :
+   */
+
+
+  /*
+   * fonction : lettreHasard -> 1 lettre au hasard
+   * données : 
+   * résultats : 
+   */
+
+
+  /*
+   * fonction : afficherMotCrypte -> afficher mot crypté
+   * données : 
+   * résultats : 
+   */
+
+
+  /*
+   * fonction jouerPartieHasard : jouer une partie au hasard
+   * données : 
+   * résultats : 
+   */
+
+
+  /*
+   * saisie d'un car au clavier
+   * données : aucune -> paramètre -> aucun
+   * résultats : un caractère -> valeur de retour -> char
+   */
+  public static char saisieClavier() {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("un caractère ? : ");
+    return sc.next().charAt(0);
+  }
+
+  /*
+   * jouer une partie interactive
+   * données : 
+   * résultats : 
+   */
+  
+}
